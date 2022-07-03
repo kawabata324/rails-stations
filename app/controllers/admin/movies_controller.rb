4c623de4056c54d65d3c60cfff22182f2abe6c25
kawabata <s1931115@s.konan-u.ac.jp>
2022-07-03 22:58:25 +0900

    update

---
+++ app/controllers/admin/movies_controller.rb
@@ -0,0 +1,27 @@
+class Admin::MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    pp movie_params
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      flash[:success] = "映画を追加しました"
+      redirect_to action: "index"
+    else
+      flash.now[:danger] = "映画の登録に失敗しました"
+      render :new
+    end
+  end
+
+  private
+
+    def movie_params
+      params.require(:movie).permit(:name, :year, :is_showing, :description, :image_url)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get '/movies', to: 'movies#index'
+    get '/movies/new', to: 'movies#new'
+    post '/movies', to: 'movies#create'
+  end
+  get '/movies', to: 'movies#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
