b33e0d0ff4aabfdea764f874ca1ce027f47601e3
kawabata <s1931115@s.konan-u.ac.jp>
2022-07-04 09:31:44 +0900

    [Update]

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get '/movies', to: 'movies#index'
-    get '/movies/new', to: 'movies#new'
-    post '/movies', to: 'movies#create'
-  end
+  # namespace :admin do
+  #   get '/movies', to: 'movies#index'
+  #   get '/movies/new', to: 'movies#new'
+  #   post '/movies', to: 'movies#create'
+  # end
   get '/movies', to: 'movies#index'
+  get '/admin/movies', to: 'movies#index'
+  # get '/admin/movies/new', to: 'admin::movies#new'
+  # post '/admin/movies', to: 'admin::movies#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
