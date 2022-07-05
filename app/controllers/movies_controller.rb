f0aa39c129d714377eea58e53be013c2f610c505
kawabata <s1931115@s.konan-u.ac.jp>
2022-07-06 07:58:36 +0900

    [Update]検索機能の実装

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,28 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+
+    @movies = if movie_params[:keyword].present?
+      Movie.where("name LIKE ?", "%#{movie_params[:keyword]}%")
+    else
+      Movie.all
+    end
+
+    case movie_params[:is_show_type]
+    in "ALL"
+      @movies
+    in 1
+      @movies.where!(is_showing: 1)
+    in 0
+      @movies.where!(is_showing: 0)
+    else
+      @movies
+    end
+
   end
+
+  private
+
+    def movie_params
+      params.permit(:keyword, :is_show_type)
+    end
 end
