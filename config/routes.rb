7ca3ddf8b0f654405549055f974b118473dd74a4
kawabata <s1931115@s.konan-u.ac.jp>
2022-07-08 13:21:16 +0900

    [Update] 送信btn formの作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
     resources :schedules, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   end
 
-  resources :movies, only: [:index, :show]
+  resources :movies, only: [:index, :show], shallow: true  do
+    resources :reservation
+  end
   get '/sheets', to: 'sheets#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
